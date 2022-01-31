326b7a2aaeaa738168f97ca7ca0a899d10291644
mayegg4869 <mayegg4869@gmail.com>
2022-01-31 17:34:56 +0900

    移植

---
+++ 220115/extra2/extra2.php
@@ -0,0 +1,20 @@
+<?php
+//参考ドキュメント
+//https://blog-and-destroy.com/13146
+
+$extra2 = [
+    [1],
+    [2,3],
+    [4,5,6],
+    [7,8]
+    ];
+
+echo count($extra2[0]);
+    echo "<br>";
+echo count($extra2[1]);
+    echo "<br>";
+echo count($extra2[2]);
+    echo "<br>";
+echo count($extra2[3]);
+
+?>
