61236641f479e4e3f0f15e32146d0d280271f486
mayegg4869 <mayegg4869@gmail.com>
2022-02-04 11:58:13 +0900

    修正

--- 220115/extra1/extra1.php
+++ 220115/extra1/extra1.php
@@ -3,16 +3,16 @@
 //https://qiita.com/fantm21/items/603cbabf2e78cb08133e
 //https://miyachi-web.com/google-books-apis/
 
-$url = "https://www.googleapis.com/books/v1/volumes?q=%E5%A4%8F%E7%9B%AE%E6%BC%B1%E7%9F%B3";
+$url = "volumes_1.json";
 $json = file_get_contents($url);
 $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
 $arr = json_decode($json,true);
 
 
 //以下課題extra1部分
-$extra1_1_title = $arr["items"][1]["volumeInfo"]["title"];
-$extra1_1_authors = $arr["items"][1]["volumeInfo"]["authors"][0];
-$extra1_1_pagecount = $arr["items"][1]["volumeInfo"]["pageCount"];
+$extra1_1_title = $arr["items"][2]["volumeInfo"]["title"];
+$extra1_1_authors = $arr["items"][2]["volumeInfo"]["authors"][0];
+$extra1_1_pagecount = $arr["items"][2]["volumeInfo"]["pageCount"];
 
 $extra1_2_title = $arr["items"][3]["volumeInfo"]["title"];
 $extra1_2_published = $arr["items"][3]["volumeInfo"]["publishedDate"];
@@ -27,7 +27,7 @@ echo "タイトル： $extra1_2_title <br>";
 echo "出版日： $extra1_2_published <br>";
 echo "プレビュ－リンク： $extra1_2_previewLink <br>";
 echo "<br>extra1-3<br>";
-echo "<a href='volumes.json'>volumes.json</a><br>";
+echo "<a href='volumes_2.json'>volumes_2.json</a><br>";
 echo "<a href='https://www.googleapis.com/books/v1/volumes?q=title:%E6%B1%9F%E6%88%B8%E5%B7%9D%E4%B9%B1%E6%AD%A9%E4%BD%9C%E5%93%81&maxResults=1'>https://www.googleapis.com/books/v1/volumes?q=title:%E6%B1%9F%E6%88%B8%E5%B7%9D%E4%B9%B1%E6%AD%A9%E4%BD%9C%E5%93%81&maxResults=1</a><br>";
 
 ?>
