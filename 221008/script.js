d8bda860802f16e9879f242d1087fb95ef284886
mayegg4869 <mayegg4869@gmail.com>
2022-10-22 01:24:06 +0900

    アニメーションの改善

--- 221008/script.js
+++ 221008/script.js
@@ -2,6 +2,7 @@ let bat1 = document.getElementById('bat_1');
 let bat2 = document.getElementById('bat_2');
 let bat3 = document.getElementById('bat_3');
 let wrap = document.getElementById('wrap');
+let witch = document.getElementById('witch');
 let particle = document.getElementById('particle');
 window.addEventListener('scroll',function(){
     let measure = window.scrollY;
@@ -10,11 +11,13 @@ window.addEventListener('scroll',function(){
         bat1.classList.add('on_1');
         bat2.classList.add('on_2');
         bat3.classList.add('on_3');
+        witch.classList.add('on');
         wrap.style.setProperty('background-color', 'rgb(46, 26, 43)', 'important');
     } else {
         bat1.classList.remove('on_1');
         bat2.classList.remove('on_2');
         bat3.classList.remove('on_3');
+        witch.classList.remove('on');
         wrap.style.setProperty('background-color', 'rgb(244, 178, 103)', 'important');
     }
 });
